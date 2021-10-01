5a014be1bb5ad19328fafd309efe91001f401325
Mustafa Çağatay Yılmaz <cagatay.yilmaz@softtech.com.tr>
2021-10-01 17:48:13 +0300

    Updated index page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BaseComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseComponent from './components/BaseComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaseComponent
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/BaseComponent.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <h1>Data Dictionary</h1>
+    <table class="table table-hover">
+      <thead>
+      <tr>
+        <th scope="col">
+          <select class="form-control">
+            <option>Please select...</option>
+          </select>
+        </th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+        <th scope="col"></th>
+      </tr>
+      <tr>
+        <th scope="col">Table</th>
+        <th scope="col">Column</th>
+        <th scope="col">Data Type</th>
+        <th scope="col">Description</th>
+        <th scope="col"></th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>account</td>
+        <td>id</td>
+        <td>int</td>
+        <td>Description</td>
+        <td>
+          <button type="button" data-toggle="modal" data-target="#updateModal" class="btn btn-success btn-sm">Düzenle</button>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+  <!-- Modal -->
+  <div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Update Description</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="form-group">
+              <label for="description">Description</label>
+              <input type="text" class="form-control" id="description" aria-describedby="description">
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
